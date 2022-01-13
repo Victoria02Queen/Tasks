b99c91a039bf1ff427fa044c0a7bd789771fb055
Viktoria <koroleva_vs02@mail.ru>
2022-01-13 15:11:12 +0300

    2 задание с уравнением

---
+++ src/com/company/peremenn/task2.java
@@ -0,0 +1,13 @@
+package com.company.peremenn;
+
+import static java.lang.Math.sqrt;
+
+public class task2 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = 7;
+        System.out.println  ( ( ( ( (a * a) - 3  ) * b * 1.0) / 44) - ( (11* 1.0)) / (a * c) ) ;
+    }
+
+}
