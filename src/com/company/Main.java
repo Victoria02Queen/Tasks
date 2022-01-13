df2db180731a53a070bbd8716e52f678d3053ae4
Viktoria <koroleva_vs02@mail.ru>
2022-01-13 14:37:50 +0300

    1 задание про товары

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,10 @@ public class Main {
     public static void main(String[] args) {
 	// ewhehfwe
         /*hi*/
+        float rubls = (float) 123.4;
+        int a = 5;
+        int b = 4;
+        System.out.println(a * 1.0 / b);
+
     }
 }
---
+++ src/com/company/peremenn/task1.java
@@ -0,0 +1,13 @@
+package com.company.peremenn;
+
+public class task1 {
+    /**/
+    public static void main(String[] args) {
+        int a = 1000;
+        int b = 500;
+        double discount = 0.1;
+        System.out.println ((a + b) * (discount)); //сумма скидки
+        System.out.println (1500 - (a + b) *  (discount)); //сумма товаров со скидкой
+
+    }
+}
