f803a0fef65d9c7411544a3d7a50a79d4b636f41
Viktoria <koroleva_vs02@mail.ru>
2022-01-17 20:34:18 +0300

    задание 1 про нахождение минимума

---
+++ src/com/company/vyrazhenie/task1.java
@@ -0,0 +1,19 @@
+package com.company.vyrazhenie;
+
+public class task1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        int c = 7;
+        int min = c;
+        if (a < c) {
+            min = a;
+        }
+
+        if (b < min) {
+            min = b;
+        }
+
+        System.out.println(min);
+    }
+    }
