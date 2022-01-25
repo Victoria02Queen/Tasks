adc73f47a6d56a2115562bb93edb5e0514e4ca40
Viktoria <koroleva_vs02@mail.ru>
2022-01-25 09:51:15 +0300

    задание 5 вычислить сумму цифр числа

---
+++ src/com/company/cycle/task5.java
@@ -0,0 +1,27 @@
+package com.company.cycle;
+
+public class task5 {
+    public static void main(String[] args) {
+        /*
+        Вычислить сумму цифр числа
+         */
+        int number = 745;
+        int sum = 0;
+        int x ;
+
+        while (number > 0) {
+            x = number % 10; // получится от остатка от деления 5
+            sum = sum + x; //получаем 5 + 0
+
+            number = number / 10; // останется 74
+
+            x = number % 10; // 74%10 останется 4
+            sum = sum + x; //получаем 5 + 4 двух //последних чисел сумма
+
+            number = number / 10; // останется 7
+
+
+        }
+        System.out.println(sum);
+    }
+}
