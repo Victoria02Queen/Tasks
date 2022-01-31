28d6381dfb53835c238a8b2c453a425d41b30aef
Viktoria <koroleva_vs02@mail.ru>
2022-01-31 14:48:21 +0300

    задание 2 от 25.01 найти первое число, большее n

---
+++ src/com/company/cycle/task2_2.java
@@ -0,0 +1,16 @@
+package com.company.cycle;
+
+public class task2_2 {
+    public static void main(String[] args) {
+        /*
+        Среди чисел 1..100, ... найти первое число, большее n.
+         */
+        int i = 0;
+        int n = 110;
+
+        while ( i <= 100 && i <= n ){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
