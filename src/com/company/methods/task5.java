13117d01749692fb49d951f94ec3003cb3b61c9f
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-02-15 18:14:38 +0300

    template

--- src/com/company/methods/task5.java
+++ src/com/company/methods/task5.java
@@ -2,20 +2,22 @@ package com.company.methods;
 
 public class task5 {
     public static void main(String[] args) {
-        wrapResult(Multiplication(6,2));
+        int mRes = Multiplication(1, 2);
+        int dRes = Division(6, 7);
+
+        System.out.printf("Result = %d; %d\n", mRes, dRes);
 
     }
-    static int Multiplication (int x,int y){
+
+    static int Multiplication(int x, int y){
         return x * y;
     }
 
-    static int Division  (int x,int y){
+    static int Division(int x, int y){
         return x / y;
     }
 
-    static void wrapResult(int x){
-
-        int res = Division(21,3);
-        System.out.println("Result = " + x + "; " + res);
+    static double calculate(int a, int b, ?){
+        ....
     }
 }
