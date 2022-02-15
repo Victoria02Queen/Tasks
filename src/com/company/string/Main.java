ac55375d234005198ac6fb50a5cfd601b1a1b9d9
Alexander Nesterov <41343903+AlexanderNest@users.noreply.github.com>
2022-02-15 19:07:46 +0300

    template

---
+++ src/com/company/string/Main.java
@@ -0,0 +1,24 @@
+package com.company.string;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int []a = {1, 2, 3, 4};
+
+        for (int i = 0; i < a.length; i++){
+            System.out.println(a[i]);
+        }
+
+
+
+
+
+
+        String line = "ывфарьчфывртчкр23947г34289173т4837т4324#####@@@";
+        String s  = "hello";
+
+        char ch = line.charAt(0);
+
+        System.out.println(line);
+    }
+}
