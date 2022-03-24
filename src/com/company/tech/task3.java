d502e3677668d321e83720f2b561eae110ba2234
Viktoria <koroleva_vs02@mail.ru>
2022-03-24 11:18:01 +0300

    задание 3 реализовать иерархию классов из простых фигур (квадрат ромб и круг)

---
+++ src/com/company/tech/task3.java
@@ -0,0 +1,13 @@
+package com.company.tech;
+
+public class task3 {
+    public static void main(String[] args) {
+        /*
+        реализовать иерархию классов из простых фигур (квадрат ромб и круг)
+         */
+        class Shape{}
+        class Circle extends Shape{}
+        class Rectangle extends Shape{}
+        class Square extends Shape{}
+    }
+}
