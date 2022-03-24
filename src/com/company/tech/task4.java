892b638414e9835498e214103bca2dd07382edcd
Viktoria <koroleva_vs02@mail.ru>
2022-03-24 11:18:14 +0300

    задание 3 реализовать иерархию классов животных абстрактные

---
+++ src/com/company/tech/task4.java
@@ -0,0 +1,17 @@
+package com.company.tech;
+
+public class task4 {
+    public abstract class Animal {
+    }
+
+    public class Mammal extends Animal {
+    }
+
+    public class Reptile extends Animal {
+    }
+
+    public class Dog extends Mammal {
+    }
+
+
+}
