bce6d0e4863d57d9eb5cf501fffa12ad751e6075
Viktoria <koroleva_vs02@mail.ru>
2022-03-24 11:18:29 +0300

    задание 5 иерархию из 4 классов сделаем классы интерфейсные

---
+++ src/com/company/tech/task5.java
@@ -0,0 +1,16 @@
+package com.company.tech;
+
+public class task5 {
+    public interface Animal {
+    }
+
+    public class Mammal implements Animal {
+    }
+
+    public class Reptile implements Animal {
+    }
+
+    public class Dog extends Mammal {
+    }
+
+}
