3676ffdbb7ae0384f16d1d400f896e605f0fc159
Viktoria <koroleva_vs02@mail.ru>
2022-03-30 21:41:09 +0300

    задание 2 Поменять местами первый и последний элементы

--- src/com/company/matrix/task2.java
+++ src/com/company/matrix/task2.java
@@ -2,17 +2,26 @@ package com.company.matrix;
 
 public class task2 {
     public static void main(String[] args) {
-        int[][] array = {{1,2}, {3,4}};
+        //Дан целочисленный двумерный массив, размерности n х m. Поменять местами первый и последний элементы каждой строки
 
-        int n = 2;
+        int m[][] = {{4, 9, 7, 2}, // ввод в матрицу
+                {4, 7, 6, 5},
+                {3, 2, 1, 8},
+                {9, 9, 7, 7}};
 
+        // обмен элемента между первым и последним столбцом
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                array[i][j] *= n;
-                System.out.print(array[i][j] + "\t"); //"\t" - табуляция
-            }
-            System.out.println(); // на другую строку
+        for (int i = 0; i < m.length; i++) {  // проход тока по столбцам
+            int t = m[i][0];  //Fist element
+            m[i][0] = m[i][m.length - 1]; //Last element
+            m[i][m.length - 1] = t;
+        }
+
+        // вывод замененной матрицы
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m.length; j++)
+                System.out.print(m[i][j] + " ");
+            System.out.println();
         }
     }
 }
