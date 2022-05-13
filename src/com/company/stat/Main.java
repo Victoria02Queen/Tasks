0752b6ab7d63ef05d119dc3e6edc63dd3b2ab05f
Viktoria <koroleva_vs02@mail.ru>
2022-05-13 23:36:06 +0300

    Подсчет радиуса

---
+++ src/com/company/stat/Main.java
@@ -0,0 +1,11 @@
+package com.company.stat;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(Test.areaCircle(4));
+
+
+
+    }
+}
---
+++ src/com/company/stat/Test.java
@@ -0,0 +1,10 @@
+package com.company.stat;
+
+public class Test {
+    final static double PI = 3.14;
+
+    public static double areaCircle(int radius){
+        return PI * radius * radius;
+    }
+
+}
