7978428951a8a5d5a87f57cbe406b6ed8978e444
viktoriakoroleva <koroleva_vs02@mail.ru>
2022-06-06 14:48:03 +0300

    внесены изменения на нахождение угла и плоашди разностороннего треугольника

--- src/com/company/tech/Triangle.java
+++ src/com/company/tech/Triangle.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Введите сторону a=");
         int a = sc.nextInt();
         System.out.println("Введите сторону b=");
@@ -20,28 +20,39 @@ public class Triangle {
 
         if ((a >= b + c) || (b >= a + c) || (c > a + b)) {
             System.out.println("Увы,такого треугольника быть не может :(");
-        } else if (a == b & b == c) {
-            System.out.println("- Равносторонний треугольник -");
+            findArea(a, b, c);
         } else if (a != b & b != c & a != c) {
             System.out.println("- Разносторонний треугольник -");
-        } else
+            findArea(a, b, c);
+        } else if (a == b & b == c) {
+            System.out.println("- Равносторонний треугольник -");
+            double v = (a * a *  Math.sqrt(3) )/ 4 ;
+            System.out.print("Площадь Равносторонний = ");
+            System.out.printf("%.2f", v);
+        } else {
             System.out.println("- Равнобедренный треугольник -");
-        
-        if (c * c == a * a + b * b){
+        }
+
+        if (c * c == (a * a + b * b)){
             System.out.println("- Прямоугольный -");
-        } else if (c * c < a * a + b * b) {
+        } else if (c * c < (a * a + b * b)) {
             System.out.println("- Остроугольный -");
-        } else if (c * c > a * a + b * b) {
+        } else if (c * c > (a * a + b * b)) {
             System.out.println("- Тупоугольный -");
         }
 
-        int p = ( a + b + c ) / 2;
-        System.out.println("Периметр треугольника = ");
-        System.out.print(p);
+
+    }
+
+    private static void findArea(int a, int b, int c) {
+        int p = (a + b + c) / 2;
+        System.out.print("Периметр треугольника = ");
+        System.out.println(p);
 
         double s = Math.sqrt(p * (p - a) * (p - b) * (p - c));
 
         System.out.print("Площадь треугольника = ");
-        System.out.printf("%.2f",s);
+        System.out.printf("%.2f", s);
     }
 }
+
